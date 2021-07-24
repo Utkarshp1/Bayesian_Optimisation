ce7a6ef2ebaadd05e0bd1cba2aca8ae3f4620e0e
Utkarshp1 <mikuprakashmiku@yahoo.com>
2021-07-24 20:05:12 +0530

    Added the ObjectiveFunction class and accordingly modified the Branin function class.

--- Branin.py
+++ Branin.py
@@ -1,7 +1,7 @@
 import torch
-from ax import ParameterType, RangeParameter, SearchSpace
+from ObjectiveFunction import ObjectiveFunction
 
-class Branin():
+class Branin(ObjectiveFunction):
     '''
         This class implements the Branin function, a simple benchmark
         function in two dimensions. This function is defined as follows:
@@ -10,87 +10,22 @@ class Branin():
             https://www.sfu.ca/~ssurjano/branin.html
     '''
     def __init__(self, noise_mean=None, noise_variance=None, 
-        random_state=None):
-        '''
-            Arguments:
-            ---------
-                - noise_mean: Mean of the normal noise to be added to 
-                    the function and gradient values
-                - noise_variance: Variance of the normal noise to be
-                    added to the function and gradient values
-                - random_state: Equivalent PyTorch manual_seed
-        '''
+        random_state=None, negate=False):
         
-        self.noise_mean = noise_mean
-        self.noise_variance = noise_variance
-        
-        # torch.manual_seed(random_state)
-        
-    def forward(self, params, *args):
+        super().__init__(noise_mean, noise_variance, random_state, negate)
+                
+    def evaluate_true(self, X):
         '''
-            This method calculates the value of the Branin function.
+            This function calculates the value of the Branin function
+            without any noise.
             
-            Arguments:
-            ---------
-                - params: (dict) A dict of mapping params to their
-                    values. The input is similar to Ax SimpleExperiment
-                    evaluation_function input format.
-                    https://ax.dev/api/core.html#simpleexperiment
+            For more information, refer to the ObjectiveFunction class
+            docs.
         '''
-        
-        self.x1, self.x2 = params["x1"], params["x2"]
-        self.x1 = torch.tensor(self.x1, requires_grad=True)
-        self.x2 = torch.tensor(self.x2, requires_grad=True)
-        
         torch.pi = torch.acos(torch.zeros(1)).item() * 2
         
-        self.y = ((self.x2 - 5.1/(4*torch.pi**2)*self.x1**2 + 
-                  5*self.x1/torch.pi - 6)**2 + 
-                  10*(1- 1/(8*torch.pi))*torch.cos(self.x1) + 10)
-        
-        return {'branin': (self.y.item(), 0.0)}
-        
-    def backward(self):
-        '''
-            This method calculates the gradient of the Branin 
-            function at the points where the function value was
-            evaluated during forward pass.
-        '''
-        self.y.backward()
-        
-        self.gradients = torch.tensor([
-            self.x1.grad,
-            self.x2.grad])
-            
-    def set_sample_space(self, params):
-        '''
-            This method sets the search space where the objective
-            function needs to optimized. The search space is of the
-            type Ax SearchSpace.
-            
-            Arguments:
-            ---------
-                - params: (dict) A dict containing keys are the name of
-                    the parameter and value being a tuple of two values
-                    in the format (lower_bound, upper_bound).
-                    
-                    For example, if we want to optimize the function 
-                    with two parameters namely x1 and x2 in the domain
-                    x1 in [-5, 10] and x2 in [0, 15], then params 
-                    argument would be like:
-                        {"x1": (-5, 10),
-                         "x2": (0, 15)}
-        '''
-        parameters = []
-        
-        for param_name, param_bounds in params.items():
-            parameters.append(
-                RangeParameter(
-                    name=param_name, 
-                    parameter_type=ParameterType.FLOAT,
-                    lower=param_bounds[0],
-                    upper=param_bounds[1]
-                )
-            )
-        
-        self.search_space = SearchSpace(parameters=parameters)
+        self.f_x = ((X[:, 1] - 5.1/(4*torch.pi**2)*X[:, 0]**2 + 
+                 5*X[:, 0]/torch.pi - 6)**2 +
+                 10*(1- 1/(8*torch.pi))*torch.cos(X[:, 0]) + 10)
+                                  
+        return self.f_x
---
+++ ObjectiveFunction.py
@@ -0,0 +1,103 @@
+import torch
+
+class ObjectiveFunction(torch.nn.Module):
+    '''
+        This class is an abstract class for an objective function that
+        is to be maximised using Bayesian Optimisation.
+        
+        Source:
+        https://botorch.org/api/_modules/botorch/test_functions/base.html
+    '''
+    
+    def __init__(self, noise_mean=0, noise_variance=None, 
+        random_state=None, negate=False):
+        '''
+            Arguments:
+            ---------
+                - noise_mean: Mean of the normal noise to be added to 
+                    the function and gradient values
+                - noise_variance: Variance of the normal noise to be
+                    added to the function and gradient values
+                - random_state: Equivalent PyTorch manual_seed
+                - negate: Multiplies the value of the function obtained
+                    with -1. Use this to minimize the Objective Function.
+        '''
+        
+        super().__init__()
+        self.noise_mean = noise_mean
+        self.noise_variance = noise_variance
+        self.negate = negate
+        
+        if random_state:
+            torch.manual_seed(random_state)
+            
+    def evaluate_true(self, X):
+        '''
+            This function calculates the value of the objective function
+            at X without noise. X is a tensor of shape batch_size x d,
+            where d is the dimension of the input taken by the Objective
+            Function.
+        '''
+        raise NotImplementedError
+        
+            
+    def forward(self, X, noise=True):
+        '''
+            This function calculates the value of the Objective 
+            Function at X with some Gaussian Noise added. The shape of X
+            is batch_size x d, where d is the dimension of the input
+            taken by the Objective Function.
+            
+            Arguments:
+            ---------
+                - X: A PyTorch tensor of shape (batch_size, d)
+                - noise (boolean): Indicates whether noise should be
+                    added.
+                    
+            Returns:
+            -------
+                - A PyTorch Tensor of shape (batch_size).
+        '''
+        
+        self.X = X.detach().clone()
+        
+        batch = self.X.ndimension() > 1
+        self.X = self.X if batch else self.X.unsqueeze(0)
+        
+        self.X.requires_grad = True
+        if self.X.grad:
+            self.X.grad.data.zero_()
+        
+        self.f_x = self.evaluate_true(X=self.X)
+        with torch.no_grad():
+            if noise and self.noise_variance is not None:
+                self.f_x += (self.noise_variance * torch.randn_like(self.f_x) +
+                    self.noise_mean)
+                
+        if self.negate:
+            self.f_x = -self.f_x
+            
+        return self.f_x if batch else self.f_x.squeeze(0)
+        
+    def backward(self, noise=True):
+        '''
+            This function calculates the gradients at X i.e. the points
+            where the Objective Function value was calculated during the
+            forward pass.
+            
+            Arguments:
+            ---------
+                - noise (boolean): Indicates whether noise should be
+                    added.
+        '''
+        
+        external_grad = torch.tensor([1.]*self.X.shape[0])
+        self.f_x.backward(gradient=external_grad)
+        
+        self.grads = self.X.grad
+        
+        if noise and self.noise_variance is not None:
+            self.grads += (self.noise_variance * torch.randn_like(self.grads)
+                    + self.noise_mean)
+        
+        return self.grads
